238cff29a789e0ee35e4aaceab04d92212991c56
Matthew Gardner <mgardner@kenzanmedia.com>
2016-12-29 16:35:58 -0500

    Added test-node-pipeline and test task

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,53 @@
 var gulp = require('gulp');
 var taskListing = require('gulp-task-listing');
 var validatePipeline = require('pipeline-validate-js');
+var testPipeline = require('pipeline-test-node')({ plugins: {
+  mocha: {
+    reporter: 'spec'
+  },
+  istanbul: {
+    includeUntested: true,
+    reporters: ['text-summary'],
+    thresholds: {
+      global: 80
+    }
+  }
+}});
+
+var validateConfig = {
+  linter: {
+    files: [
+      '*.js',
+      './src/*.js',
+      './test/**/*.js'
+    ]
+  },
+  test: {
+    files: [
+      '*.js',
+      './src/*.js',
+      './test/**/*.js',
+    ]
+  }
+};
+
+
 
 gulp.task('help', taskListing);
 
 gulp.task('lint', function() {
   gulp.src([
-    './gulpfile.js'
+    validateConfig.linter.files
   ]).pipe(validatePipeline.validateJS());
 });
 
 gulp.task('build', ['lint']);
+
+
+gulp.task('test', function() {
+  return gulp
+    .src(validateConfig.test.files)
+    .pipe(testPipeline.test());
+});
 
 gulp.task('default', ['help']);
